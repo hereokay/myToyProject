fc99060c989ef9cdb02ed1b9fc00c3c2a66f1e0b
Chris <hereokay88@gmail.com>
2024-05-03 19:51:31 +0900

    refactor: fetch, extract, handle 메서드 함수 분리

--- main.js
+++ main.js
@@ -4,6 +4,46 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+
+
+async function fetchBlockTxFromInfura(blockNumber, address) {
+    const response = await axios.post(url, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_getBlockByNumber",
+        params: [blockNumber, true]
+    });
+    // TODO
+    // 오류처리
+    return response.data.result.transactions;
+}
+
+function extractTotal(transactions){
+    let totalBalanceChange = BigInt(0);
+    let totalFee = BigInt(0);
+
+    for(i=0;i<transactions.length;i++){
+        const tx = transactions[i];
+        
+        // 출금 또는 컨트랙트 내역
+        // from, to 가 같을경우 출금내역에 등록
+        if(tx.from == address.toLowerCase()){
+            totalBalanceChange -= BigInt(tx.value);
+            const fee = BigInt(tx.gas) * BigInt(tx.gasPrice);
+            totalFee += fee;
+            console.log(tx);
+        }
+        // 입금내역
+        else if(tx.to == address.toLowerCase()){
+            totalBalanceChange += BigInt(tx.value);
+            const fee = BigInt(tx.gas) * BigInt(tx.gasPrice);
+            totalFee += fee;
+            console.log(tx);
+        }
+    }
+    return [totalBalanceChange, totalFee];
+}
+
 // http://localhost:3000/transactions?address=0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5&blockNumber=0x12dd669
 // curl "http://localhost:3000/transactions?address=0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5&blockNumber=0x12dd669"
 app.get('/transactions', async (req, res) => {
@@ -12,41 +52,10 @@ app.get('/transactions', async (req, res) => {
         return res.status(400).send('Both address and blockNumber are required');
     }
 
-    const url = `https://mainnet.infura.io/v3/${process.env.API_KEY}`;
-
-    try {
-        const response = await axios.post(url, {
-            jsonrpc: "2.0",
-            id: 1,
-            method: "eth_getBlockByNumber",
-            params: [blockNumber, true]
-        });
+    try {        
+        const transactions = fetchBlockTxFromInfura(blockNumber,address);
         
-        const transactions = response.data.result.transactions;
-        let totalBalanceChange = BigInt(0);
-        let totalFee = BigInt(0);
-
-        for(i=0;i<transactions.length;i++){
-
-            const tx = transactions[i];
-            
-            // 출금 또는 컨트랙트 내역
-            // from, to 가 같을경우 출금내역에 등록
-            if(tx.from == address.toLowerCase()){
-                totalBalanceChange -= BigInt(tx.value);
-                const fee = BigInt(tx.gas) * BigInt(tx.gasPrice);
-                totalFee += fee;
-                console.log(tx);
-            }
-            // 입금내역
-            else if(tx.to == address.toLowerCase()){
-                totalBalanceChange += BigInt(tx.value);
-                const fee = BigInt(tx.gas) * BigInt(tx.gasPrice);
-                totalFee += fee;
-                console.log(tx);
-            }
-        
-        }
+        [totalBalanceChange, totalFee] = extractTotal(transactions);
 
         res.json({
             "balanceChange" : totalBalanceChange.toString(10),
